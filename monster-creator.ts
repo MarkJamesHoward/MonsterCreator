186e779ddce229784f26291fd7abdcbc8e99df6f
MarkHoward <markjameshoward@hotmail.co.uk>
2018-09-14 22:30:03 +1200

    customise

--- monster-creator.ts
+++ monster-creator.ts
@@ -7,68 +7,124 @@ import * as Gestures from "@polymer/polymer/lib/utils/gestures";
 
 //@ts-ignore
 class MonsterCreator extends GestureEventListeners(LitElement) {
-  @property()
-  selectedEyeX;
-  @property()
-  selectedEyeY;
-  @property()
-  selectedEyeIndex;
-  @property()
-  selectedMouthIndex;
-  @property()
-  selectedMouthX;
-  @property()
-  selectedMouthY;
-  @property()
-  selectedSilhouetteIndex;
-  @property()
-  eyes = 1;
-  @property()
-  mouth = 1;
-  @property()
-  silhouette = 1;
-  @property()
-  NUMBER_OF_IMAGES_EYES = 4;
-  @property()
-  NUMBER_OF_IMAGES_MOUTH = 5;
-  @property()
-  NUMBER_OF_IMAGES_SILHOUETTES = 5;
-  @property()
-  locks = [];
-  @property()
+  @property({ type: Number, reflect: true, attribute: true })
+  selectedeyex;
+  @property({ type: Number, reflect: true, attribute: true })
+  selectedeyey;
+  @property({ type: Number, reflect: true, attribute: true })
+  selectedmouthindex;
+  @property({ type: Number, reflect: true, attribute: true })
+  selectedmouthx;
+  @property({ type: Number, reflect: true, attribute: true })
+  selectedmouthy;
+  @property({ type: Number, reflect: true, attribute: true })
+  selectedsilhouetteindex;
+  @property({ type: Number })
+  NUMBER_OF_IMAGES_EYES = 3;
+  @property({ type: Number })
+  NUMBER_OF_IMAGES_MOUTH = 3;
+  @property({ type: Number })
+  NUMBER_OF_IMAGES_SILHOUETTES = 4;
+  @property({ type: Number, attribute: true })
   level = 1;
-  @property({ type: Boolean })
-  customize = true;
-  @property()
+  @property({ type: String })
+  customize = "Yes";
+  @property({ type: Boolean, attribute: false })
   EyesSlot1Used = false;
-  @property()
+  @property({ type: Boolean, attribute: false })
   EyesSlot2Used = false;
-  @property()
+  @property({ type: Boolean, attribute: false })
   EyesSlot3Used = false;
-  @property()
+  @property({ type: Boolean, attribute: false })
   EyesSlot4Used = false;
-  @property()
+  @property({ type: Boolean, attribute: false })
   MouthSlot2Used = false;
-  @property()
+  @property({ type: Boolean, attribute: false })
   MouthSlot3Used = false;
-  @property()
+  @property({ type: Boolean, attribute: false })
   MouthSlot4Used = false;
-  @property()
+  @property({ type: Boolean, attribute: false })
   MouthSlot1Used = false;
+  @property({ type: Number, reflect: true, attribute: true })
+  selectedeyeindex;
+
+  firstUpdated() {
+    console.log("first updated called");
+    this.ready();
+  }
 
   constructor() {
     super();
-    console.log("created2");
+    this.level = 1;
   }
 
   ready() {
     console.log("ready called");
-    console.log("sil " + this.silhouette);
-    console.log("customil " + this.customize);
 
-    console.dir(this.shadowRoot.querySelector("#level"));
+    let node: any;
 
-    console.log("afet null");
+    switch (this.selectedmouthindex) {
+      case 1:
+        this.MouthSlot1Used = true;
+        //@ts-ignore
+        node = this.shadowRoot.querySelector("#MouthSlot1");
+        node.style.left = this.selectedmouthx + "%";
+        node.style.top = this.selectedmouthy + "%";
+        break;
+      case 2:
+        this.MouthSlot2Used = true;
+        //@ts-ignore
+        node = this.shadowRoot.querySelector("#MouthSlot2");
+        node.style.left = this.selectedmouthx + "%";
+        node.style.top = this.selectedmouthy + "%";
+        break;
+      case 3:
+        this.MouthSlot3Used = true;
+        //@ts-ignore
+        node = this.shadowRoot.querySelector("#MouthSlot3");
+        node.style.left = this.selectedmouthx + "%";
+        node.style.top = this.selectedmouthy + "%";
+        break;
+      case 4:
+        this.MouthSlot4Used = true;
+        //@ts-ignore
+        node = this.shadowRoot.querySelector("#MouthSlot4");
+        node.style.left = this.selectedmouthx + "%";
+        node.style.top = this.selectedmouthy + "%";
+        break;
+    }
+
+    switch (this.selectedeyeindex) {
+      case 1:
+        this.EyesSlot1Used = true;
+        //@ts-ignore
+        node = this.shadowRoot.querySelector("#EyesSlot1");
+        node.style.left = this.selectedeyex + "%";
+        node.style.top = this.selectedeyey + "%";
+        break;
+      case 2:
+        this.EyesSlot2Used = true;
+        //@ts-ignore
+        node = this.shadowRoot.querySelector("#EyesSlot2");
+        node.style.left = this.selectedeyex + "%";
+        node.style.top = this.selectedeyey + "%";
+        break;
+      case 3:
+        this.EyesSlot3Used = true;
+        //@ts-ignore
+        node = this.shadowRoot.querySelector("#EyesSlot3");
+        node.style.left = this.selectedeyex + "%";
+        node.style.top = this.selectedeyey + "%";
+        break;
+      case 4:
+        this.EyesSlot4Used = true;
+        //@ts-ignore
+        node = this.shadowRoot.querySelector("#EyesSlot4");
+        node.style.left = this.selectedeyex + "%";
+        node.style.top = this.selectedeyey + "%";
+        break;
+    }
+
     Gestures.addListener(
       //@ts-ignore
       this.shadowRoot.querySelector("#EyesSlot1"),
@@ -199,10 +255,6 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
   handleTrackEye(e: any, item: any, index: Number) {
     //@ts-ignore
     let container = this.shadowRoot.querySelector("#eyesandsilcontainer");
-    // console.log(container.clientWidth);
-    // console.log(e.detail.sourceEvent.clientX);
-    // console.log("handling track " + e.detail.state);
-    // console.dir(e);
     switch (index) {
       case 1:
         this.EyesSlot2Used = false;
@@ -243,11 +295,11 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
         break;
       case "end":
         console.log("trackng ended - store position");
-        this.selectedEyeIndex = index;
-        this.selectedEyeX = item.style.left;
-        this.selectedEyeY = item.style.top;
-        console.log(item.trackX);
-        console.log(item.trackY);
+        this.selectedeyeindex = index;
+        this.selectedeyex = item.style.left.substring(0, 2);
+        this.selectedeyey = item.style.top.substring(0, 2);
+        console.log("selected eye index " + this.selectedeyeindex);
+        console.log("selected eye X " + this.selectedeyex);
         break;
     }
   }
@@ -294,13 +346,11 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
         break;
       case "end":
         console.log("tracking ended - store position and selected index");
-        item.trackX = item.style.left;
-        item.trackY = item.style.top;
-        this.selectedMouthIndex = index;
-        this.selectedMouthX = item.style.left;
-        this.selectedMouthY = item.style.top;
-        console.log(item.trackX);
-        console.log(item.trackY);
+        this.selectedmouthindex = index;
+        this.selectedmouthx = item.style.left.substring(0, 2);
+        this.selectedmouthy = item.style.top.substring(0, 2);
+        console.log("selected mouth index " + this.selectedmouthindex);
+        console.log("selected mouth X " + this.selectedmouthx);
         break;
     }
   }
@@ -344,18 +394,18 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
       .CharacterCustomizeMain {
         padding: 0;
         margin: 0;
-        display: grid;
+        /* display: grid;
         grid-template-areas: "Top"
                               "Bottom";
         grid-template-rows: 1fr 1fr;
-        grid-template-columns: 1fr;
+        grid-template-columns: 1fr; */
       }
           
       .SilhouetteBackgroundContainer {
         align-self: top;
         grid-area: Top;
-        width: 100%;
-        height: 30%;
+        /* width: 100%;
+        height: 30%; */
       }
 
      #SilhouetteSelector {
@@ -370,7 +420,7 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
       }
     
       .silhouette {
-        grid-area: silhouette;
+        /* grid-area: silhouette; */
         justify-content: center;
         align-items: flex-end;
         display: flex;
@@ -421,17 +471,17 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
       width: 20%;
     }
     
-    #eyesandsilcontainer {
+    /* #eyesandsilcontainer {
       width: 100%;
-    }
+    } */
 
     </style>
 
-    <button @click="${() => {
+    <button class="${
+      this.customize === "Yes" ? "" : "hidden"
+    }" @click="${() => {
       this.LevelUp();
     }}" id="level">Level++</button>
-
-    <div>Level: ${this.level}</div>
 
     <div class="CharacterCustomizeMain">
 
@@ -440,90 +490,153 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
         <div id="eyesandsilcontainer" style="position:relative">  
 
           <div class="eyes">
-            <div id="EyesSlot1" class="${
-              this.EyesSlot1Used ? "EyesSlot1" : "Eye1ResetPostion"
+            <div class="${
+              this.customize != "Yes" && this.EyesSlot1Used == false
+                ? "DisableCustomize"
+                : ""
             }">
-              <slot name="EyesSlot1" >I need eyes please!</slot>
-            </div>
-
-            <div id="EyesSlot2" class="${
-              this.EyesSlot2Used ? "EyesSlot2" : "Eye2ResetPostion"
-            }">
-              <slot name="EyesSlot2" >I need eyes please!</slot>
-            </div>
-
-            <div id="EyesSlot3" class="${
-              this.EyesSlot3Used ? "EyesSlot3" : "Eye3ResetPostion"
-            }">
-              <slot name="EyesSlot3" >I need eyes please!</slot>
-            </div>
-
-            <div class="${this.level > 3 ? "" : "hidden"}">
-              <div id="EyesSlot4" class="${
-                this.EyesSlot4Used ? "EyesSlot4" : "Eye4ResetPostion"
+              <div id="EyesSlot1" class="${
+                this.EyesSlot1Used ? "EyesSlot1" : "Eye1ResetPostion"
               }">
-                <slot name="EyesSlot4" >I need eyes please!</slot>        
+                <slot name="EyesSlot1" ></slot>
               </div>
             </div>
+
+            <div class="${
+              this.customize != "Yes" && this.EyesSlot2Used == false
+                ? "DisableCustomize"
+                : ""
+            }">
+              <div id="EyesSlot2" class="${
+                this.EyesSlot2Used ? "EyesSlot2" : "Eye2ResetPostion"
+              }">
+                <slot name="EyesSlot2" ></slot>
+              </div>
+            </div>
+
+            <div class="${
+              this.customize != "Yes" && this.EyesSlot3Used == false
+                ? "DisableCustomize"
+                : ""
+            }">
+              <div id="EyesSlot3" class="${
+                this.EyesSlot3Used ? "EyesSlot3" : "Eye3ResetPostion"
+              }">
+                <slot name="EyesSlot3" ></slot>
+              </div>
+            </div>
+
+            <div class="${
+              this.customize != "Yes" && this.EyesSlot4Used == false
+                ? "DisableCustomize"
+                : ""
+            }">
+              <div class="${this.level > 3 ? "" : "hidden"}">
+                <div id="EyesSlot4" class="${
+                  this.EyesSlot4Used ? "EyesSlot4" : "Eye4ResetPostion"
+                }">
+                  <slot name="EyesSlot4" ></slot>        
+                </div>
+              </div>
+            </div>
+
+
           </div>
 
           <div class='mouth'>
+
+          <div class="${
+            this.customize != "Yes" && this.MouthSlot1Used == false
+              ? "DisableCustomize"
+              : ""
+          }">
               <div class="${this.level > 0 ? "" : "hidden"}">
                 <div id="MouthSlot1" class="${
                   this.MouthSlot1Used ? "MouthSlot1" : "Mouth1ResetPostion"
                 }">
-                  <slot name="MouthSlot1" >I need eyes please!</slot>        
+                  <slot name="MouthSlot1" ></slot>        
                 </div>
               </div>
+          </div>
 
-              <div class="${this.level > 0 ? "" : "hidden"}">
+          <div class="${
+            this.customize != "Yes" && this.MouthSlot2Used == false
+              ? "DisableCustomize"
+              : ""
+          }">
+              <div class="${this.level > 1 ? "" : "hidden"}">
                 <div id="MouthSlot2" class="${
                   this.MouthSlot2Used ? "MouthSlot2" : "Mouth2ResetPostion"
                 }">
-                  <slot name="MouthSlot2" >I need eyes please!</slot>        
+                  <slot name="MouthSlot2" ></slot>        
                 </div>
               </div>
+          </div>
 
-              <div class="${this.level > 0 ? "" : "hidden"}">
+          <div class="${
+            this.customize != "Yes" && this.MouthSlot3Used == false
+              ? "DisableCustomize"
+              : ""
+          }">
+              <div class="${this.level > 2 ? "" : "hidden"}">
                 <div id="MouthSlot3" class="${
                   this.MouthSlot3Used ? "MouthSlot3" : "Mouth3ResetPostion"
                 }">
-                  <slot name="MouthSlot3" >I need eyes please!</slot>        
+                  <slot name="MouthSlot3" ></slot>        
                 </div>
               </div>
+          </div>
               
-              <div class="${this.level > 0 ? "" : "hidden"}">
+          <div class="${
+            this.customize != "Yes" && this.MouthSlot4Used == false
+              ? "DisableCustomize"
+              : ""
+          }">
+              <div class="${this.level > 3 ? "" : "hidden"}">
                 <div id="MouthSlot4" class="${
                   this.MouthSlot4Used ? "MouthSlot4" : "Mouth4ResetPostion"
                 }">
-                  <slot name="MouthSlot4" >I need eyes please!</slot>        
+                  <slot name="MouthSlot4" ></slot>        
                 </div>
               </div>
+          </div>
         
           </div>
-          
-          <div class="${this.silhouette === 1 ? "" : "hidden"}">
-            <slot name="pickedsilhouette1">I need a body please!</slot>
+      
+        <div class="OverlayTwoItemsCharacter">
+          <div style="grid-area:main" class="${
+            this.selectedsilhouetteindex == 1 ? "" : "hidden"
+          }">
+            <slot name="pickedsilhouette1"></slot>
           </div>
-          <div class="${this.silhouette === 2 ? "" : "hidden"}">
-            <slot name="pickedsilhouette2">I need a body please!</slot>
+          <div style="grid-area:main" class="${
+            this.selectedsilhouetteindex == 2 ? "" : "hidden"
+          }">
+            <slot name="pickedsilhouette2"></slot>
           </div>
-          <div class="${this.silhouette === 3 ? "" : "hidden"}">
-            <slot name="pickedsilhouette3">I need a body please!</slot>
+          <div style="grid-area:main" class="${
+            this.selectedsilhouetteindex == 3 ? "" : "hidden"
+          }">
+            <slot name="pickedsilhouette3"></slot>
           </div>
-          <div class="${this.silhouette === 4 ? "" : "hidden"}">
-            <slot name="pickedsilhouette4">I need a body please!</slot>
+          <div  style="grid-area:main" class="${
+            this.selectedsilhouetteindex == 4 ? "" : "hidden"
+          }">
+            <slot name="pickedsilhouette4"></slot>
           </div>
-          <div class="${this.silhouette === 5 ? "" : "hidden"}">
-            <slot name="pickedsilhouette5">I need a body please!</slot>
+          <div style="grid-area:main" class="${
+            this.selectedsilhouetteindex == 5 ? "" : "hidden"
+          }">
+            <slot name="pickedsilhouette5"></slot>
           </div>
+        </div>
 
         </div>
 
       </div>
 
       <div id='SilhouetteSelector' class="${
-        this.customize ? "silhouette" : "DisableCustomize"
+        this.customize == "Yes" ? "silhouette" : "DisableCustomize"
       }">
       
         <div @click="${() =>
@@ -565,7 +678,7 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
           this.Pick(5)}" class="OverlayTwoItemsCharacter silhouettePicker">
           <slot name="silhouette5"><div class="MissingImage">Please supply Silhouette5.png</div></slot>
           <iron-icon class="${
-            this.level >= 4 ? "NoPadlock" : ""
+            this.level >= 5 ? "NoPadlock" : ""
           }" style="grid-area:main;z-index:2;align-self:center;justify-self:center"
             icon="lock"></iron-icon>
         </div>
@@ -579,8 +692,7 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
   Pick(e) {
     console.log(e);
     if (e <= this.level) {
-      this.silhouette = e;
-      this.selectedSilhouetteIndex = e;
+      this.selectedsilhouetteindex = e;
     }
   }
 
