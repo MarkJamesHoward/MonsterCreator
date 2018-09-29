ae81bc893daf9ea553e31d02fc321c4fb486a410
MarkHoward <markjameshoward@hotmail.co.uk>
2018-09-30 09:28:47 +1300

    svg updates

--- monster-creator.ts
+++ monster-creator.ts
@@ -7,6 +7,9 @@ import {
 import { GestureEventListeners } from "@polymer/polymer/lib/mixins/gesture-event-listeners";
 import * as Gestures from "@polymer/polymer/lib/utils/gestures";
 
+import { tween, styler, easing } from "popmotion";
+import { interpolate } from "flubber";
+
 // import "@polymer/iron-icons/iron-icons.js";
 // import "@polymer/paper-icon-button";
 
@@ -15,6 +18,7 @@ import * as Gestures from "@polymer/polymer/lib/utils/gestures";
 class MonsterCreator extends GestureEventListeners(LitElement) {
   @property({ type: Number, reflect: true, attribute: true })
   selectedeyex;
+  y;
   @property({ type: Number, reflect: true, attribute: true })
   selectedeyey;
   @property({ type: Number, reflect: true, attribute: true })
@@ -440,18 +444,15 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
         height: 100%;
       }
 
+      ::slotted(svg)  {
+        width: 300px;
+      } 
+
       ::slotted(img) {
         width:50%;
         max-width: 150px;
         object-fit: contain;
       } 
-
-      ::slotted(img.SilhouetteSlot) {
-        width:50%;
-        max-width: 300px;
-        display: block; 
-        margin: 0 auto!important;
-      }
 
 
     .MouthSlot1, .MouthSlot2 , .MouthSlot3, .MouthSlot4 {
@@ -612,7 +613,7 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
         <div style="grid-area:main;  margin: 0 auto" class="${
           this.selectedsilhouetteindex == 5 ? "" : "hidden"
         }">
-          <slot style="margin: 0 auto"  name="pickedsilhouette5"></slot>
+          <slot id="target"  style="margin: 0 auto"  name="pickedsilhouette5"></slot>
         </div>
       </div>
 
@@ -657,9 +658,31 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
             icon="lock"></iron-icon>
         </div>
 
-        <div @click="${() =>
-          this.Pick(5)}" class="OverlayTwoItemsCharacter silhouettePicker">
-          <slot name="silhouette5"><div class="MissingImage">Please supply Silhouette5.png</div></slot>
+        <div @click="${() => {
+          let star = "";
+          this.DetermineShapeToMorphFrom(star);
+          console.log(star);
+          const shape = styler(this.querySelector("#pickedsilhouette5"), null);
+          const circle =
+            "M86,171.5 C38.7796539,171.5 0.5,133.220346 0.5,86 C0.5,38.7796539 38.7796539,0.5 86,0.5 C133.220346,0.5 171.5,38.7796539 171.5,86 C171.5,133.220346 133.220346,171.5 86,171.5 Z";
+
+          console.log("test");
+          tween({
+            duration: 2000,
+            ease: easing.easeInOut
+          })
+            .pipe(interpolate(star, circle, { maxSegmentLength: 2 }))
+            .start(shape.set("d"));
+
+          this.Pick(5);
+        }}" 
+        class="OverlayTwoItemsCharacter silhouettePicker">
+         
+          <slot name="silhouette5">
+            <div class="MissingImage">
+            Please supply Silhouette5.png
+            </div>
+          </slot>
           <iron-icon class="${
             this.level >= 5 ? "NoPadlock" : ""
           }" style="grid-area:main;z-index:2;align-self:center;justify-self:center"
@@ -669,6 +692,18 @@ class MonsterCreator extends GestureEventListeners(LitElement) {
 
     </div>
         `;
+  }
+
+  DetermineShapeToMorphFrom(star) {
+    if (this.selectedsilhouetteindex === 4) {
+      star =
+        "M39.812 43.617c-1.576.629-3.814 1.368-5.396.44-1.628-.953-1.817-3.11-1.897-4.819 1.11.71 2.637 1.378 3.667.18.068-.078.028-.185-.067-.216-2.73-.884-4.117-3.846-3.288-6.53.178-.578 3.149-7.777 2.645-11.027-.466-3.009-1.892-4.76-4.51-6.096-2.808-1.432-6.517-1.869-9.418-.458-2.734 1.33-4.357 4.084-4.6 7.07-.271 3.311 1.829 6.085 2.763 9.111.235.763.871 3.13.896 4.33.035 1.67-.497 3.391-1.36 4.856-.799 1.356-2.395 2.305-3.981 2.03a.132.132 0 0 0-.12.224c.998 1.005 2.607 1.093 3.723.279-.397 1.845-1.598 3.4-3.273 4.295-1.18.63-2.56.822-3.641-.09-.93-.784-1.239-2.078-1.527-3.202-.035-.138-.214-.102-.252.01-.628 1.822-.093 3.816 1.297 5.133 1.37 1.297 3.395 1.61 5.161 1.109 2.06-.585 3.597-2.264 4.772-3.967.969-1.404 1.887-2.952 2.373-4.61a43.429 43.429 0 0 1-.749 5.967c-.436 2.215-1.216 5.351-3.987 5.495-.155.008-.16.213-.024.259 2.584.856 5.132-.693 6.413-2.912 1.049-1.817 1.249-3.903 1.163-5.98.436 1.841 1.156 3.582 2.584 4.93 1.769 1.67 4.752 2.05 6.545.211.094-.096-.013-.25-.136-.214-1.898.564-3.723-.659-4.686-2.24-1.014-1.665-1.03-3.776-.985-5.699.89 2.12 2.19 4.369 4.489 5.048 2.262.668 4.647-.618 5.56-2.73.043-.096-.038-.234-.154-.187";
+    } else if (this.selectedsilhouetteindex === 3) {
+      star =
+        "M103.04 162.52L39.36 196l12.16-70.9L0 74.86 71.2 64.5 103.04 0l31.85 64.52 71.2 10.35-51.57 50.22L166.7 196z";
+    } else {
+      star = "M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0";
+    }
   }
 
   Pick(e) {
